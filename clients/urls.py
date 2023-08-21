fc04093235eea4b40b8dfdb4d4eba52f88775888
William Lowrimore <wlowrimore@gmail.com>
2023-08-20 21:00:15 -0500

    fixes redirect bug in create clients

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import view_clients, create_client
+from .views import view_clients, search_clients, create_client
 
 urlpatterns = [
     path('', view_clients, name='view_clients'),
-    path('create_client', create_client, name='create_client')
+    path('create_client', create_client, name='create_client'),
+    path('search_clients', search_clients, name='search_clients'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.urls import reverse
 
 from .models import Client
 from .forms import ClientForm
@@ -13,13 +12,25 @@ def view_clients(request):
     return render(request, 'view_clients.html', context)
 
 
+def search_clients(request):
+    query_dict = request.GET
+    query = query_dict.get("q")
+    client_obj = None
+    if query is not None:
+        client_obj = Client.objects.get(email=query)
+    context = {
+        "client": client_obj
+    }
+    return render(request, "search_clients.html", context)
+
+
 def create_client(request):
     if request.method == 'POST':
         form = ClientForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'The client was successfully added!')
-            return redirect(reverse('home'))
+            return redirect('view_clients')
         context = {'form': form}
         return render(request, 'create_client.html', context)
 
